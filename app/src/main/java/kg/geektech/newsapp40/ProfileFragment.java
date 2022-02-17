74189588e77ad9d5244e7d8104803e3e12cc9fa0
Abdulla08 <abdullazakipov8@gmail.com>
2022-02-17 18:26:15 +0600

    Initial commit

---
+++ app/src/main/java/kg/geektech/newsapp40/ProfileFragment.java
@@ -0,0 +1,50 @@
+package kg.geektech.newsapp40;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kg.geektech.newsapp40.databinding.FragmentProfileBinding;
+
+public class ProfileFragment extends Fragment {
+    private FragmentProfileBinding binding;
+    ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri uri) {
+                    // Handle the returned Uri
+                    binding.imageView.setImageURI(uri);
+                }
+            });
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentProfileBinding.inflate(inflater,  container, false);
+        return binding.getRoot();
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGetContent.launch("image/*");
+            }
+        });
+    }
+}
